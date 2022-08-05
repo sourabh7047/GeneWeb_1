ef34ee99bc47e8a1ab54f5186301875906f2c72a
sourabh7047 <sourabhasharma15@gmail.com>
2022-08-06 00:34:55 +0530

    just to test

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("project-geneweb/build"));
 }
 
+app.get("/master", (req,res)=>{
+  res.send("this is it")
+})
+
 // ---------------------------listen requests
 
 app.listen(PORT, function () {
