44bb111bfbac800a4b14a0a9ed2684a7483bf8ef
sourabh7047 <sourabhasharma15@gmail.com>
2022-08-06 15:19:37 +0530

    path imported

--- server.js
+++ server.js
@@ -17,12 +17,13 @@ const app = express();
 app.use(cors());
 app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // ----------------------------get requests
 
 app.use("/internal", NcbiRoutes);
 app.use("/toolname", EbiRoutes);
+console.log(__dirname)
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
