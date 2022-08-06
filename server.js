5974c581d886c690e9c61bdad4e07f15769a3b6a
sourabh7047 <sourabhasharma15@gmail.com>
2022-08-06 14:45:26 +0530

    path imported

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
   app.use("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client/build/index.html'))
   });
 }
 
